ef37808c9278a51147357b43a15a28206b52171a
Olivier DELALANDE <odelalande@KLF-GFB58H2.kaliop.loc>
2019-04-08 11:36:01 +0200

    Ajout de la logique pour que les FS puissent rajouter un niveau eidas

--- helpers/utils.js
+++ helpers/utils.js
@@ -8,12 +8,12 @@ import config from '../config';
 
 
 export const getAuthorizationUrlForAuthentication = (eidasLevel) => {
-	const eidas = eidasLevel ? `&acr_values=${eidasLevel}` : ''
-	return `${config.FC_URL}${config.AUTHORIZATION_FC_PATH}?`
+  const eidas = eidasLevel ? `&acr_values=${eidasLevel}` : '';
+  return `${config.FC_URL}${config.AUTHORIZATION_FC_PATH}?`
   + `response_type=code&client_id=${config.AUTHENTICATION_CLIENT_ID}&redirect_uri=${config.FS_URL}`
   + `${config.LOGIN_CALLBACK_FS_PATH}&scope=${config.MANDATORY_SCOPES} ${config.FC_SCOPES}&state=home&nonce=customNonce11`
   + `${eidas}`;
-}
+};
 
 export const getAuthorizationUrlForData = () => `${config.FC_URL}${config.AUTHORIZATION_FC_PATH}?`
   + `response_type=code&client_id=${config.DATA_CLIENT_ID}&redirect_uri=${config.FS_URL}`
