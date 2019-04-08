334de42cefb82f95aa5574eadab95b614b62e942
Olivier DELALANDE <odelalande@KLF-GFB58H2.kaliop.loc>
2019-04-08 16:45:13 +0200

    Ajout de la logique pour que les FS puissent rajouter un niveau eidas

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,7 @@
 /* eslint-env browser */
 
 function toggleData() { // eslint-disable-line no-unused-vars
-  var display = document.getElementById('resdetails').style.display;
+  let { display } = document.getElementById('resdetails').style;
   if (!display || display === 'none') {
     document.getElementById('resdetails').style.display = 'block';
     document.getElementById('accessData').innerText = 'Cacher les informations renvoyées par FranceConnect';
