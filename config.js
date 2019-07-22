6f0f9f96df8a817c8d5c6e9ac2d9fe9ac02ff5b6
Stéphane <stephane.herman@live.fr>
2019-07-22 13:15:35 +0200

    [FCP-397] New URL for data-provider
    https://fournisseur-de-donnees.dev-franceconnect.fr

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   FC_URL: process.env.FC_URL || 'https://fcp.integ01.dev-franceconnect.fr',
-  FD_URL: process.env.FD_URL || 'https://data-provider-example.herokuapp.com',
+  FD_URL: process.env.FD_URL || 'https://fournisseur-de-donnees.dev-franceconnect.fr',
   FS_URL: process.env.FS_URL || 'http://localhost:3000',
   AUTHENTICATION_CLIENT_ID: process.env.CLIENT_ID || '211286433e39cce01db448d80181bdfd005554b19cd51b3fe7943f6b3b86ab6e',
   AUTHENTICATION_CLIENT_SECRET: process.env.CLIENT_SECRET || '2791a731e6a59f56b6b4dd0d08c9b1f593b5f3658b9fd731cb24248e2669af4b',
