6db966e35fb9a8f292e678aa7d9ad6d9451b4356
Arnaud Dev JS <apalin@kaliopmail.com>
2019-11-22 18:03:57 +0100

    [FCP-549] Add login param validator

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,26 +1,57 @@
+import Joi from '@hapi/joi';
 import config from '../config';
+import { QUERY_ERROR_REGEX } from '../helpers/utils';
+
 /**
  *  Display basic login page with possible errors from auth callback
  * @param {Express.req} req
  * @param {Express.res} res
  */
 export const getLogin = (req, res) => {
-  /**
-   * OpenID Connect standard errors
-   * @see @link{https://www.rfc-editor.org/rfc/rfc6749.html#section-4.1.2.1}
-   */
-  const {
-    query: {
-      error,
-      error_description: errorDescription,
-    },
-  } = req;
+  // 1 - get only the interesting params
+  const { query, params, body } = req;
+  const inputs = { query, params, body };
 
-  const data = {};
-  if (error) {
-    const errorTitle = config.OPENID_ERRORS[error] || 'erreur inconnue';
-    Object.assign(data, { error: errorTitle, errorDescription });
+  // 2 - define how the params should be.
+  const loginSchema = Joi.object({
+    query: {
+      error: Joi.string()
+        .valid(...Object.keys(config.OPENID_ERRORS))
+        .optional(),
+      error_description: Joi.string()
+        .regex(QUERY_ERROR_REGEX)
+        .optional(),
+    },
+    body: Joi.object().length(0),
+    params: Joi.object().length(0),
+  });
+
+  // 3 - validate the inputs
+  const { error: inputsError, value } = loginSchema.validate(inputs);
+
+  // 4 - if the validation failed, this is a bad request
+  if (inputsError) {
+    const status = 400;
+    return res.status(status).render('pages/error/4xx.ejs', {
+      status,
+      error: 'Bad request',
+      errorDescription: "La requête n'est pas correctement formattée",
+    });
   }
 
-  return res.render('pages/login', data);
+  // 5 - we grab the meaningful params
+  const {
+    query: { error, error_description: errorDescription },
+  } = value;
+
+  // 6 - we edit the login with error
+  const data = {};
+  let status = 200;
+  if (error) {
+    status = 400;
+    const errorTitle = config.OPENID_ERRORS[error] || 'erreur inconnue';
+    Object.assign(data, { status, error: errorTitle, errorDescription });
+  }
+
+  return res.status(status).render('pages/login', data);
 };
