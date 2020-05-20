e97b8fa495ca2dcd19a8b221fa06d31af60b1a94
Arnaud Dev JS <apalin@kaliopmail.com>
2020-05-20 05:58:35 +0200

    [fc-core-22] fix: update eslint && add ignored directories for CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: 'airbnb-base',
+  ignorePatterns: ["node_modules/*","public/js/*",'.eslintrc.js'],
   rules: {
     'import/prefer-default-export': 0,
+    'arrow-parens':0,
   }
 };
